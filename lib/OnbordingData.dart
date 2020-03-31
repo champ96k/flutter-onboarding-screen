f620d6e466814042f095df48d6a331ee6743268e
Tushar Nikam <champ96k@gmail.com>
2020-03-31 16:02:46 +0530

    first commit

---
+++ lib/OnbordingData.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class OnbordingData extends StatefulWidget {
+  final imagePath;
+  final title;
+  final desc;
+
+  OnbordingData({this.imagePath, this.title, this.desc});
+
+  @override
+  _OnbordingDataState createState() =>
+      _OnbordingDataState(this.imagePath, this.title, this.desc);
+}
+
+class _OnbordingDataState extends State<OnbordingData> {
+  final imagePath;
+  final title;
+  final desc;
+  _OnbordingDataState(this.imagePath, this.title, this.desc);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image(
+            fit: BoxFit.fitWidth,
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height*0.40,
+            image: AssetImage(imagePath),
+          ),
+
+          SizedBox(
+            height: 12.0,
+          ),
+
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                title,
+                softWrap: true,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 12.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              desc,
+              softWrap: true,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/flutteronboardingscreens.dart
@@ -0,0 +1,121 @@
+library flutteronboardingscreens;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_screen/OnbordingData.dart';
+
+/// A IntroScreen Class.
+///
+///
+class IntroScreen extends StatefulWidget {
+  final List<OnbordingData> onbordingDataList;
+  final MaterialPageRoute pageRoute;
+  IntroScreen(this.onbordingDataList, this.pageRoute);
+
+  void skipPage(BuildContext context) {
+    Navigator.push(context, pageRoute);
+  }
+
+  @override
+  IntroScreenState createState() {
+    return new IntroScreenState();
+  }
+}
+
+class IntroScreenState extends State<IntroScreen> {
+  final PageController controller = new PageController();
+  int currentPage = 0;
+  bool lastPage = false;
+
+  void _onPageChanged(int page) {
+    setState(() {
+      currentPage = page;
+      if (currentPage == widget.onbordingDataList.length - 1) {
+        lastPage = true;
+      } else {
+        lastPage = false;
+      }
+    });
+  }
+
+  Widget _buildPageIndicator(int page) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 2.0),
+      height: page == currentPage ? 10.0 : 6.0,
+      width: page == currentPage ? 10.0 : 6.0,
+      decoration: BoxDecoration(
+        color: page == currentPage ? Colors.blue : Colors.grey,
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: new Color(0xFFEEEEEE),
+      padding: const EdgeInsets.all(10.0),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new Expanded(
+            flex: 1,
+            child: new Container(),
+          ),
+          new Expanded(
+            flex: 3,
+            child: new PageView(
+              children: widget.onbordingDataList,
+              controller: controller,
+              onPageChanged: _onPageChanged,
+            ),
+          ),
+          new Expanded(
+            flex: 1,
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                new FlatButton(
+                  child: new Text(lastPage ? "" : "SKIP",
+                      style: new TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0)),
+                  onPressed: () => lastPage
+                      ? null
+                      : widget.skipPage(
+                    context,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Container(
+                    child: Row(
+                      children: [
+                        _buildPageIndicator(0),
+                        _buildPageIndicator(1),
+                        _buildPageIndicator(2),
+                      ],
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  child: new Text(lastPage ? "GOT IT" : "NEXT",
+                      style: new TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0)),
+                  onPressed: () => lastPage
+                      ? widget.skipPage(context)
+                      : controller.nextPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeIn),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
